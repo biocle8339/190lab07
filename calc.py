2e5fd651618cb220b423b467fa9ee1edf99c08ef
Wonho <lee2058@purdue.edu>
2015-10-08 13:53:23 -0400

    add mod

--- calc.py
+++ calc.py
@@ -33,6 +33,10 @@ def mult(a, b):
 def div(a, b):
     return a / b
 
+#cs190 lab07
+def mod(a, b):
+    return a % b
+
 
 # -------------------------------------------------------- #
 
@@ -67,6 +71,8 @@ while (True):
             print "Product: ", mult(a, b)
         elif (op == "/"):
             print "Quotient: ", div(a, b)
+        elif (op == "%"):
+            print "Mod: ", mod(a,b)
         else:
             print "Invalid operation..."
 
